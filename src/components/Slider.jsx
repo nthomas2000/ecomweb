246fb1fec8cb68d47bac69341c23ced48134f53d
nthomas2000 <nitinkuthukallunkal@gmail.com>
2022-10-09 13:19:40 +0530

    added clickhandler for slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,16 @@
 import { ArrowLeftOutlined, ArrowRightOutlined, Info } from "@material-ui/icons"
+import { useState } from "react";
 import styled from "styled-components"
+import { sliderItems } from "../data";
 
 const Container = styled.div`
 width:100%;
 height:100vh;
 background-color:white;
 display:flex;
-position:relative;`;
+position:relative;
+
+`;
 
 
 const Arrow = styled.div`
@@ -24,43 +28,91 @@ left: ${(props)=> props.direction === "left" && "10px"};
 right: ${(props)=> props.direction === "right" && "10px"};
 margin:auto;
 cursor:pointer;
-opacity:0.5`;
+opacity:0.5;
+`;
 
 const Wrapper = styled.div`
+height:100%;
+display:flex;
+transform:translateX(${props=>props.slideIndex*-100}vw    );
+
+`;
+
+const Slide= styled.div`
 width:100vw;
 height:100vh;
 display:flex;
 align-items:center;
-margin:10px;
+background-color :#${props=>(props.bg)}
 `
 
 const ImageContainer = styled.div`
-flex:1`
+flex:1;
+height:100%;
+`
 const InfoContainer =styled.div`
 flex:1;
-margin-left:100px;
+padding:50px;
+
+
 `
 const Img=styled.img`
+height:80%;
 width:100%
 `
+const Title =styled.h1`
+font-size:70px;
+font-weight:500;`
+const Desc =styled.p`
+margin: 50px 0px;
+font-size:20px;
+font-weight:500;
+letter-spacing:3px;`
+const Button =styled.button`
+background-color:black;
+align:center;
+color:white;
+padding:10px;`
 
 const Slider = () => {
+    const[slideIndex,setSlideIndex]=useState(0);
+    const handleClick =(direction) => {
+        
+        if (direction==="left"){
+            setSlideIndex(slideIndex >0 ? slideIndex-1 :2);
+
+        }else{
+            setSlideIndex(slideIndex <2 ? slideIndex+1 :0);
+        }
+    }
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>handleClick("left")}>
             <ArrowLeftOutlined/>
         </Arrow>
-        <Wrapper>
+        
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map((items)=>(
+        <Slide bg={items.bg}>
             <ImageContainer>
-                <Img src= "R.png"/>
+                <Img src= {items.img}/>
             </ImageContainer>
-            <InfoContainer>THIS IS IT</InfoContainer>
+            <InfoContainer>
+                <Title>{items.title}</Title>
+                <Desc>{items.desc}</Desc>
+                <Button>GAME ON</Button>
+            </InfoContainer>
+            </Slide>
+ ) )}
         </Wrapper>
+      
 
-        <Arrow direction ="right">
+        <Arrow direction ="right" onClick={()=>handleClick("right")}>
             <ArrowRightOutlined/>
-            
-        </Arrow>
+            </Arrow>
+     
+        
+    
     </Container>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "R.png",
+        title: "WINTER SALE",
+        desc: "SHOW BEFORE DRAGONS SHOW UP",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "R.png",
+        title: "WINTER SALE",
+        desc: "SHOW BEFORE DRAGONS SHOW UP",
+        bg: "f5fafu",
+    },
+    {
+        id: 3,
+        img: "R.png",
+        title: "WINTER SALE",
+        desc: "SHOW BEFORE DRAGONS SHOW UP",
+        bg: "f5fafd",
+    }
+]
